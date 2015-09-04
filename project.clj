fe30114a5be4c337617f899a8d24ebebd9ea88ec
Ruediger Gad <rgad@fb2.fh-frankfurt.de>
2015-09-04 14:56:37 +0200

    Update dependencies.

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clj-assorted-utils "1.7.0"]
-                 [fg-netzwerksicherheit/clj-jms-activemq-toolkit "1.99.1"]
+                 [clj-assorted-utils "1.9.1"]
+                 [fg-netzwerksicherheit/clj-jms-activemq-toolkit "1.99.3"]
                  [crypto-random "1.2.0"]]
   :aot :all
   :main event-gateway.main)
