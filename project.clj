ff03393b63ee8ea5ef612851e1aedb5da2c2c6ec
Ruediger Gad <rgad@fb2.fh-frankfurt.de>
2015-09-04 14:16:18 +0200

    Use latest clj-jms-activemq-toolkit.

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [clj-assorted-utils "1.7.0"]
-                 [clj-jms-activemq-toolkit "1.1.1"]
+                 [fg-netzwerksicherheit/clj-jms-activemq-toolkit "1.99.1"]
                  [crypto-random "1.2.0"]]
   :aot :all
   :main event-gateway.main)
