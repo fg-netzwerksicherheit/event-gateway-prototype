08ddcb501b88999d43cfaa7343657874dda5f74c
Ruediger Gad <rgad@fb2.fh-frankfurt.de>
2015-09-09 15:38:09 +0200

    Add missing license information.

--- test/event_gateway/core_test_with_jms.clj
+++ test/event_gateway/core_test_with_jms.clj
@@ -1,3 +1,12 @@
+;;;
+;;;   Copyright 2015, Frankfurt University of Applied Sciences
+;;;
+;;;   This software is released under the terms of the Eclipse Public License 
+;;;   (EPL) 1.0. You can find a copy of the EPL at: 
+;;;   http://opensource.org/licenses/eclipse-1.0.php
+;;;
+
+
 (ns event-gateway.core-test-with-jms
   (:require [clojure.test :refer :all]
             [clojure.pprint :refer :all]
